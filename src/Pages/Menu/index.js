9dec827be3c3f2facf6847e8cfd431802756c1db
donleyac <happylumberjack@gmail.com>
2017-04-17 23:13:05 -0400

    Updated front page text outline, updated text on menu and redid protein setup

--- src/Pages/Menu/index.js
+++ src/Pages/Menu/index.js
@@ -9,9 +9,12 @@ import './styles.scss';
 import bannerImg from './assets/banner_wood.jpg';
 
 import cardImg1 from '../About/assets/main_salad.jpg';
-import cardImg2 from '../About/assets/md_bread.jpg';
-import cardImg3 from '../About/assets/md_fig.jpg';
-import cardImg4 from '../About/assets/md_salmon.jpg';
+import wrap from './assets/wrap.png';
+import ricebowl from './assets/ricebowl.png';
+import tabouli from './assets/tabouli.png';
+import hummus from './assets/hummus.png';
+import fries from './assets/fries.png';
+import veggies from './assets/vegetables.png';
 
 
 function CardInfo(props){
@@ -19,17 +22,15 @@ function CardInfo(props){
     <div className="card-info">
       <h1 className="title">{props.title}</h1>
       <p className="content">{props.content}</p>
-      <p className="price">{props.price}</p>
+      {/* <p className="price">{props.price}</p> */}
     </div>
   )
 }
 
 function ProteinCard(props){
-  let bgStyle={backgroundImage: 'url(' + props.image + ')'};
   return (
     <div className="protein-container">
       <p className="protein-label">{props.label}</p>
-      <div className="protein-card" style={bgStyle} />
       <p className="protein-descrip">{props.descrip}</p>
     </div>
   )
@@ -58,58 +59,50 @@ var proteinArray = [
   },
   {
     label:"Grilled Veggies",
-    image: cardImg1,
+    image: veggies,
     descrip:"Chargrilled skewered mushroom, onions, peppers, zucchini marinated in fresh herb, garlic & olive oil"
   }
 ];
 
 var cardArray = [
-  {category:'warm', image:cardImg1, info:(<CardInfo title="Pita Wrap"
+  {category:'warm', image:wrap, info:(<CardInfo title="Pita Wrap"
     content="Grilled pita bread with hand cut fries, tomatoes, lettuce,
     garlic aioli & your choice of protein or grilled veggies."
     price="$9.99"/>)},
-  {category:'warm', image:cardImg1, info:(<CardInfo title="Rice Bowl"
+  {category:'warm', image:ricebowl, info:(<CardInfo title="Rice Bowl"
     content="A bowl filled with seasoned rice vermicelli, your choice of protein or
     grilled veggies, lettuce, tomatoes, & garlic sauce"
     price="$9.99"/>)},
   {category:'warm', image:cardImg1, info:(<CardInfo title="Salad"
     content="A bowl filled with vibrant salad, choice of protein or grilled veggies."
     price="$9.99"/>)},
-  {category:'cold', image:cardImg1, info:(<CardInfo title="Hand Cut Fries"
-    content="Garlic & Herbs or Cajun"
-    price="$9.99"/>)},
-  {category:'cold', image:cardImg1, info:(<CardInfo title="Hummus"
+  {category:'cold', image:hummus, info:(<CardInfo title="Hummus"
     content="A smooth blend of chickpeas, tahini & lemon juice."
     price="$9.99"/>)},
-  {category:'cold', image:cardImg1, info:(<CardInfo title="Tabouli"
+  {category:'cold', image:tabouli, info:(<CardInfo title="Tabouli"
     content="A mix of finely diced tomatoes, onions, parsley and crushed wheat with lemon juice & olive oil vinaigrette."
     price="$9.99"/>)},
   {category:'cold', image:cardImg1, info:(<CardInfo title="Fatoush"
     content="A refreshing mix of fresh romaine lettuce, cucumber, tomatoes, fresh mint, and crispy pita croutons drizzled with lemon juice, olive oil and sumac vinaigrette "
     price="$9.99"/>)},
-  {category:'cold', image:cardImg1, info:(<CardInfo title="Side Salad"
-    content="Tossed salad with lettuce, tomatoes, cucumber and choice of homemade vinaigrette"
+  {category:'cold', image:fries, info:(<CardInfo title="Hand Cut Fries"
+    content="Garlic & Herbs or Cajun"
     price="$9.99"/>)},
-  {category:'bev', image:cardImg1, info:(<CardInfo title="Item1"
+  {category:'bev', image:cardImg1, info:(<CardInfo title="Lemon Fresca"
     content="Aliquam aliquet faucibus massa,
     nec aliquam tortor ullamcorper vel. Suspendisse
     dapibus est at libero sollicitudin lacinia. Ut at finibus dolor."
     price="$9.99"/>)},
-  {category:'bev', image:cardImg1, info:(<CardInfo title="Item1"
+  {category:'bev', image:cardImg1, info:(<CardInfo title="Jasmine Green Tea"
     content="Aliquam aliquet faucibus massa,
     nec aliquam tortor ullamcorper vel. Suspendisse
     dapibus est at libero sollicitudin lacinia. Ut at finibus dolor."
     price="$9.99"/>)},
-  {category:'bev', image:cardImg1, info:(<CardInfo title="Item1"
+  {category:'bev', image:cardImg1, info:(<CardInfo title="Matcha Fresca"
     content="Aliquam aliquet faucibus massa,
     nec aliquam tortor ullamcorper vel. Suspendisse
     dapibus est at libero sollicitudin lacinia. Ut at finibus dolor."
     price="$9.99"/>)},
-  {category:'bev', image:cardImg1, info:(<CardInfo title="Item1"
-    content="Aliquam aliquet faucibus massa,
-    nec aliquam tortor ullamcorper vel. Suspendisse
-    dapibus est at libero sollicitudin lacinia. Ut at finibus dolor."
-    price="$9.99"/>)}
 ];
 
 function MenuGenerator(props) {
@@ -148,6 +141,10 @@ export default function(props){
         <Scroll to="cold">Healthy Sides</Scroll>
         <Scroll to="bev">Beverages</Scroll>
       </AffixHeader>
+      <div className="protein-header">
+        <h1 className="sec-title">Protein Options</h1>
+        <p className="descr">Choose the core ingredient in your Classic Meal</p>
+      </div>
       <div className="protein-row">
         {proteinArray.map(function(elem){
             return(<ProteinCard label={elem.label} image={elem.image} descrip={elem.descrip}/>)
@@ -157,7 +154,7 @@ export default function(props){
         warm ingredients, all made from scratch." category="warm" />
       <MenuGenerator title="Healthy Sides" info="Made with fresh, organic produce
         that is locally sourced each season." category="cold" />
-      <MenuGenerator title="Beverages" info="Drink Responsibly." category="bev" />
+      <MenuGenerator title="Beverages" info="Drinks made fresh everyday." category="bev" />
     </div>
   )
 }
